bd6279f339238436ed28efe221aa19bf68269624
Jordan Wiseman <74243511+JWiseman-git@users.noreply.github.com>
2025-09-09 20:33:18 +0100

    latest

--- application_RAG.py
+++ application_RAG.py
@@ -7,12 +7,70 @@ The contxt provided:
 
 TODO: Build RAW LinkedIn scraper
 """
+import faiss
+from langchain_community.docstore.in_memory import InMemoryDocstore
+from langchain_community.vectorstores import FAISS
+
+from typing import List
+from langchain_core.documents import Document
 
 from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_openai import OpenAIEmbeddings
 
-file_path = "../example_data/nke-10k-2023.pdf"
+from dotenv import load_dotenv
+from pathlib import Path
+
+from langchain_community.docstore.in_memory import InMemoryDocstore
+from langchain_community.vectorstores import FAISS
+
+from langchain_core.runnables import chain
+
+env_path = Path('.') / '.env'
+load_dotenv(env_path)
+
+# Load PDF
+file_path = "./sample_data/Jordan_Wiseman_CV_AI_Engineer.pdf"
 loader = PyPDFLoader(file_path)
-
 docs = loader.load()
 
-print(len(docs))
+# Split into chunks
+text_splitter = RecursiveCharacterTextSplitter(
+    chunk_size=1000,
+    chunk_overlap=200,
+    add_start_index=True
+)
+all_splits = text_splitter.split_documents(docs)
+
+embeddings = OpenAIEmbeddings(model="text-embedding-3-large")
+
+sample_vector = embeddings.embed_query("test")
+embedding_dim = len(sample_vector)
+index = faiss.IndexFlatL2(embedding_dim)
+
+vector_store = FAISS(
+    embedding_function=embeddings,
+    index=index,
+    docstore=InMemoryDocstore(),
+    index_to_docstore_id={},
+)
+
+ids = vector_store.add_documents(documents=all_splits)
+
+results = vector_store.similarity_search_with_score(
+    "Where does Jordan live?"
+)
+doc, score = results[0]
+print(score)
+
+# Retriever Factory Method
+@chain
+def retriever(query: str) -> List[Document]:
+    return vector_store.similarity_search(query, k=1)
+
+retriever.batch(
+    [
+        "How many distribution centers does Nike have in the US?",
+        "When was Nike incorporated?",
+    ],
+)
